d2483341c94b2b0d7ebcfb02a6c0c256ecd6221c
Diego Iturra <33190592+DiegoIturra@users.noreply.github.com>
2019-06-06 12:16:39 -0400

    Add files via upload

---
+++ UVa/1124.cpp
@@ -0,0 +1,12 @@
+//UVa 1124 - Celebrity jeopardy(ACEPTADO)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+	string equation;
+	while(getline(cin,equation)){
+		cout << equation << endl;
+	}
+	return 0;
+}
