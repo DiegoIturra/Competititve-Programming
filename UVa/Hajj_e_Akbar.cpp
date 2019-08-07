8de67ba318c4d31f93dcc8c0be1d4f644ee0e4eb
Diego Iturra <33190592+DiegoIturra@users.noreply.github.com>
2019-08-07 13:25:48 -0400

    Add files via upload

---
+++ UVa/Hajj_e_Akbar.cpp
@@ -0,0 +1,17 @@
+//UVa 12577 Hajj-e-Akbar
+#include <iostream>
+using namespace std;
+
+int main(){
+	string name;
+	int numCase = 0;
+	while(true){
+		cin >> name;
+		numCase++;
+		if(name == "*") break;
+		if(name == "Hajj") cout << "Case " << numCase << ": " << "Hajj-e-Akbar" << endl; 
+		else if(name == "Umrah") cout << "Case " << numCase << ": " << "Hajj-e-Asghar" << endl; 
+	}
+	return 0;
+}
+
