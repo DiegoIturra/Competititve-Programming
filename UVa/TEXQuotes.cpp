3a209d2f86ccf69fa7b885c2f75d5106f0593850
Diego Iturra <33190592+DiegoIturra@users.noreply.github.com>
2019-11-14 21:02:15 -0300

    Add files via upload

---
+++ UVa/TEXQuotes.cpp
@@ -0,0 +1,24 @@
+//Uva 272 TEXquotes (ACEPTADO)
+#include <cstdio>
+using namespace std;
+
+int main(void){
+    int count = 2;
+    char caracter;
+    while(scanf("%c",&caracter)==1){
+        if(caracter == '"'){
+            if(count%2 == 0){
+                printf("``");
+            }
+            else if(count){
+                printf("''");
+            }
+            count++;
+        }
+        else{
+            printf("%c",caracter);
+        }
+    }
+    return 0;
+}
+
